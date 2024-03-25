a0122de3e105d843d5bce9b44769dc0323629135
Erin McCluskey <2842838m@student.gla.ac.uk>
2024-03-25 17:01:28 +0000

    chapter 7

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -3,10 +3,11 @@ from django.template.defaultfilters import slugify
 
 # Create your models here.
 class Category(models.Model):
-    name = models.CharField(max_length=128, unique=True)
+    NAME_MAX_LENGTH = 128
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
     slug = models.SlugField(unique=True)
+    name = models.CharField(max_length=NAME_MAX_LENGTH, unique=True)
 
     def save(self, *args, **kwargs):
           self.slug = slugify(self.name)
@@ -19,11 +20,14 @@ class Category(models.Model):
         return self.name
 
 class Page(models.Model): 
-	category = models.ForeignKey(Category, on_delete=models.CASCADE) 
-	title = models.CharField(max_length=128) 
-	url = models.URLField() 
-	views = models.IntegerField(default=0) 
-	
-	def __str__(self): 
-		return self.title
+    TITLE_MAX_LENGTH = 128
+    URL_MAX_LENGTH = 200
+    
+    category = models.ForeignKey(Category, on_delete=models.CASCADE) 
+    title = models.CharField(max_length=TITLE_MAX_LENGTH) 
+    url = models.URLField() 
+    views = models.IntegerField(default=0) 
+    
+    def __str__(self): 
+        return self.title
 
