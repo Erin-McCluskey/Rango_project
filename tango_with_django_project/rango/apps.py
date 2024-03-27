5d2254c40c6718927b260126b246f82e734952a3
Erin McCluskey <2842838m@student.gla.ac.uk>
2024-03-27 10:07:32 +0000

    Maybe fix

--- tango_with_django_project/rango/apps.py
+++ tango_with_django_project/rango/apps.py
@@ -1,5 +1,5 @@
-from django.apps import AppConfig
+#from django.apps import AppConfig
 
 
-class RangoConfig(AppConfig):
-    name = 'apps.rango'
+#class RangoConfig(AppConfig):
+#    name = 'apps.rango'
